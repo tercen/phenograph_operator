5ef8df7b8a33c5e56a5c74cf465831078cae5e75
lucaslelann <lucas.lelann@gmail.com>
2021-10-25 15:21:12 +0200

    Update main.R

--- main.R
+++ main.R
@@ -6,23 +6,24 @@ library(igraph)
 
 ctx <- tercenCtx()
 
-data = ctx  %>% 
+data <- ctx  %>% 
   select(.ci, .ri, .y) %>% 
   reshape2::acast(.ci ~ .ri, value.var = '.y', fill = NaN, fun.aggregate = mean) 
 
-colnames(data) = paste('c', colnames(data), sep = '')
+colnames(data) <- paste('c', colnames(data), sep = '')
 
 k <- 30
 if(!is.null(ctx$op.value('k'))) k <- as.numeric(ctx$op.value('k'))
-seed <- 42
+
+seed <- NULL
 if(!is.null(ctx$op.value('seed'))) k <- as.numeric(ctx$op.value('seed'))
 set.seed(seed)
 
-dataRpheno = Rphenograph::Rphenograph(data, k = k)
+dataRpheno <- Rphenograph::Rphenograph(data, k = k)
 
-membership_num = as.numeric(membership(dataRpheno[[2]]))
-membership_label = as.character(membership_num)
-modularity_num = modularity(dataRpheno[[2]])
+membership_num <- as.numeric(membership(dataRpheno[[2]]))
+membership_label <-sprintf(paste0("c%0", max(nchar(as.character(membership_num))), "d"), membership_num)
+modularity_num <- modularity(dataRpheno[[2]])
 
 data.frame(.ci = seq(from = 0, to = length(membership_num) - 1), membership_num, membership_label, modularity_num) %>%
   ctx$addNamespace() %>%
